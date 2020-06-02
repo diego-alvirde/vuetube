ad2963e25860bbd11f2bc22dbce98bdf01519c33
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2020-06-02 16:40:18 -0500

    Generando nuevos valores con COMPUTED properties

---
+++ componentes/computedComponentes/ComputedProperties.js
@@ -0,0 +1,40 @@
+Vue.component('computed-properties', {
+    template: `
+        <div>
+            <h1>Computed Properties</h1>
+            <p>{{ fullName }}</p>            
+            <p>{{ userAge }}</p>            
+        </div>
+    `,
+    data() {
+        return {
+            user:{
+                name:'Jesus',
+                lastName:'Lopez',
+                year:'1991'
+            }
+        }
+    },
+    computed:{
+        fullName() {
+            return `${this.user.name} ${this.user.lastName}`
+        },
+        userAge() {
+            let date = new Date()
+            let currentYear = date.getFullYear()
+
+            return currentYear - Number(this.user.year)
+        },
+    },
+    methods:{
+        nombreCompleto() {
+            return `${this.user.name} ${this.user.lastName}`
+        },
+        edad() {
+            let date = new Date()
+            let currentYear = date.getFullYear()
+
+            return currentYear - Number(this.user.year)
+        }
+    }
+})
